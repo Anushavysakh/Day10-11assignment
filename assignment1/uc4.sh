1e8706993dfe134e174c28505fd062a2909eac48
Anusha <anushavysakh232@gmail.com>
2022-07-03 16:17:19 +0530

    Compute

---
+++ assignment1/uc4.sh
@@ -0,0 +1,10 @@
+read -p "Enter first number :" a
+read -p "Enter second number :" b
+read -p "Enter third number :" c
+echo "Enter numbers are : " $a $b $c
+x=$((a+b*c))
+y=$((a*b+c))
+z=$((a%b+c))
+echo "a+b*c=" $x
+echo "a*b+c=" $y
+echo "a%b+c=" $z
