fb82c302c1ba0321c281593c44c87541a9641c70
Anusha <anushavysakh232@gmail.com>
2022-07-03 20:57:56 +0530

    New file

---
+++ assignment2/flipCombination.sh
@@ -0,0 +1,8 @@
+n=$((RANDOM%2))
+if [[ n -eq 0 ]]
+then 
+	echo "Head "
+	else
+	echo "Tail"
+fi
+
--- flipCoinStimulator.sh
+++
@@ -1,7 +0,0 @@
-number=$((RANDOM%2))
-if [[ number -eq 1 ]]
-then
-	echo "Head"
-	else
-	echo "Tail"
-fi
