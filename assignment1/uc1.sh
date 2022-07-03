ab03f977eabc3251a16b2e611f66f0f2f1aa619a
Anusha <anushavysakh232@gmail.com>
2022-07-03 16:04:35 +0530

    Read 3 numbers

---
+++ assignment1/uc1.sh
@@ -0,0 +1,5 @@
+#Write program to take three inputs a, b & c
+read -p "Enter first number :" a
+read -p "Enter second number :" b
+read -p "Enter third number :" c
+echo "Enter numbers are : " $a $b $c
