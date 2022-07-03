46bc07d8ae73efb274846d296fefe3c718f629ab
Anusha <anushavysakh232@gmail.com>
2022-07-03 16:09:18 +0530

    Compute

---
+++ assignment1/uc2.sh
@@ -0,0 +1,9 @@
+read -p "Enter first number :" a
+read -p "Enter second number :" b
+read -p "Enter third number :" c
+echo "Enter numbers are : " $a $b $c
+result=$((a+b*c))
+echo "result=" $result
+
+
+
